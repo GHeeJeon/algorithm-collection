44d09cab8164a5dfe4bcc3b2034cc916e2fce42e
GHeeJeon <jhjeon_99@naver.com>
2020-11-27 18:20:56 +0900

    create package wrapping making BST

---
+++ pythonProject/search/binary_tree_search.py
@@ -0,0 +1,32 @@
+# binary tree search with linked-list
+def connect_node():
+    return
+
+
+def insert_node():
+    connect_node()
+    return
+
+
+def delete_node():
+    connect_node()
+    return
+
+
+def print_node():
+    in_preorder()
+    in_inorder()
+    in_postorder()
+    return
+
+
+def in_preorder():
+    return
+
+
+def in_inorder():
+    return
+
+
+def in_postorder():
+    return
